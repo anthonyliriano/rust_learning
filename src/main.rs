d892d001ec73fa3198d9f4240ea13acb2e44f6d3
anthonyliriano <anthonylir@gmail.com>
2023-08-09 17:51:10 -0400

    Tuple Types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    tupleTypes();
+}
+
+fn variables(){
+    let x = 5;
+    println!("The variable is equal to: {x}");
+    let x = x + 1; //shadowing
+    println!("The variable is equal to: {x}");
+    
+}
+
+fn tupleTypes(){
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    let one = tup.2;
+    println!("The value of y is :{y}");
+    println!("The value of z is : {one}")
 }
